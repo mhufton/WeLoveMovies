42b91de8ae30cfd16044d065e20ee13a5a4a6a65
Mike Hufton <mhufton90@gmail.com>
2021-09-02 14:22:05 -0500

    updated DATABASE_URL to for knex migrations

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://hsfjvcol:7GdtXlwFOLt8Sss7XlAvgEykCoKMXygD@kashin.db.elephantsql.com/hsfjvcol",
 } = process.env;
 
 module.exports = {
